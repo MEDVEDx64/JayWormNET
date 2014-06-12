7261be89fe3a29f6363f3e1fa31b33154f68f89f
Andrey Bobkov <medvedx64@gmail.com>
2014-06-12 23:15:30 +0400

    Added example command script

---
+++ scripts/example.js
@@ -0,0 +1,16 @@
+// Example additional command script,
+//  implements IIRCAdditionalCommand.
+//
+// Add 'example' in commands.lst to check it out.
+
+function getRequiredArgsCount() {
+	return 0
+}
+
+function getPermissionLevel() {
+	return 2
+}
+
+function execute(sender, channel, args) {
+	sender.sendMessage('Workaet!\n')
+}
